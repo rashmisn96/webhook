5923747d8fec57063054c9946865f4e833df62a8
rashmisn96 <54940949+rashmisn96@users.noreply.github.com>
2020-04-17 16:38:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,337 +1,278 @@
-'use strict';
 
-const {dialogflow} = require('actions-on-google');
-const functions = require('firebase-functions');
-const { WebhookClient } = require('dialogflow-fulfillment');
-const { Card, Suggestion, QuickReplies } = require('dialogflow-fulfillment');
-const axios = require('axios');
-const admin = require('firebase-admin');
-const rp = require('request');
-const url = "https://soa.actcorp.in/api/chatbot/getOptions";
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
-const app = dialogflow({debug: true});
+Skip to content
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-	const agent = new WebhookClient({ request: request, response: response });
-	console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-	console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
-
-	function checkContext(){
-		var parameter=request.body.queryResult.parameters;
-		var contexts={};
-		var con ={};
-		var verifotp={};
-		if(parameter.input_param == ''){
-			console.log('no params');
-		} else{
-			con = agent.getContext('account_no');
-			console.log('find here');
-			console.log(con);
-			//if(1){
-			if(con==null || con==''  || !(con.parameters.value)){ 
-				var acc=  request.body.queryResult.queryText;
-			
-				return new Promise(function (resolve, reject) {
-					secondapi(agent); 
-				});
-				
-			} else {
-				console.log('call multi action api');  
-
-			}
-
-		}
-
-	}
-
-	function fallback(agent) {
-		//checkContext();
-		return new Promise(function (resolve, reject) {
-			checkContext(agent); 
-		});
-	}
+    Why GitHub?
+                          
 
 
-	function firstapi(agent) { 
-		if (request.body.originalDetectIntentRequest.source == 'facebook') {
-			// if(1){
-			var params = { source: request.body.originalDetectIntentRequest.source };
-			return new Promise(function (resolve, reject) {
-				axios.post(`https://soa.actcorp.in/api/chatbot/getOptions`, params).then((result) => {
-					console.log(result.headers);
-					var cookieValue = result.headers['set-cookie']; 
-					let cookiee=[];
-					 cookieValue.forEach(element => {
-		                
-		                 var cookieValue_array = element.split(";"); 
-		                 console.log(cookieValue_array );
-		                 var ciValue = cookieValue_array[0];
-		                 console.log(ciValue);
-		                 var cicookieValue_array = ciValue.split("="); 
-		                 console.log(cicookieValue_array); 
-		                var cicookieValue = cicookieValue_array[1];
-		                 console.log(cicookieValue);
-		             // agent.setContext({name: 'ci_session', lifespan: 4, parameters: {'ci_cookie': cicookieValue}});
-		              cookiee.push(cicookieValue);
+                    
+Team
+Enterprise
+Explore
+                      
+
+                    
+Marketplace
+Pricing
+                       
+
+
+                        
+
+Sign in
+Sign up
+SunilSyal /
+Webhook-and-API.AI-Echo-Sample
+
+10
+70
+
+    339
+
+Code
+Issues 1
+Pull requests 1
+Actions
+Projects 0
+Security
+Insights
+Join GitHub today
+
+GitHub is home to over 40 million developers working together to host and review code, manage projects, and build software together.
+Webhook-and-API.AI-Echo-Sample/index.js /
+@SunilSyal SunilSyal Update index.js 0520013 on Jul 22, 2019
+executable file 219 lines (205 sloc) 6.26 KB
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/echo", function(req, res) {
+  var speech =
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.echoText
+      ? req.body.queryResult.parameters.echoText
+      : "Seems like some problem. Speak again.";
   
-		                 
-		                 
-		                });
-		              console.log('below');
-		             		console.log(cookiee);
-		             		 
-		                    var ci_seesion=cookiee.toString(); 
-		                    console.log('above');
-		                      console.log('below');
-		                    console.log(ci_seesion); 
-		                      console.log('above');
-                  
-					var resultarray = result.data;   
-					
-					if (resultarray.code == 200 && !(resultarray.authenticate)) {
-						var fullfilment = resultarray.fulfillment;
-						fullfilment.forEach(element => {
-							var messages = element.messages;
-							messages.forEach(element => { 
-								var r = {
-										"messages": [
-											{
-												"platform": "FACEBOOK",
-												"quickReplies": {
-													"quickReplies": element.replies,
-													"title": element.title
-												}
-											}
-											]
-								};
-
-								let responseJson = {};
-								responseJson.fulfillmentMessages = r.messages;
-								responseJson.fulfillmentMessages = r.messages;
-								console.log('Response to Dialogflow: ' + JSON.stringify(responseJson));
-								response.json(responseJson);
-							});
-						});
-					}
-				}).catch(error => {
-					console.log(`Api is not working`); 
-					//agent.add(`Api is not working`);
-				});
-			});
-
-		}
-
-		else if (request.body.originalDetectIntentRequest.source == 'google') {
-			console.log(`Hey, Im your Act Google Assistant`);
-			//agent.add(`Hey, Im your Act Google Assistant`);
-		} 
-	}
-
-	function secondapi(agent) {  	
-			return new Promise(function (resolve, reject) {
-				if (request.body.originalDetectIntentRequest.source == 'facebook') {
-					var accno=request.body.queryResult.parameters.input_param;
-					console.log('check');
-					console.log(accno);
-					if(!(accno)){
-					    accno=request.body.queryResult.queryText; 
-					} 
-					console.log(accno);
-					var params={};  
-					console.log(request.body);
-					params = { source: request.body.originalDetectIntentRequest.source,data:[{input_param:accno}] };
-					console.log(params);
-					
-					/*var config = {
-							  headers: { 'Content-Type': 'application/json;charset=utf-8' },
-							  Cookie: 'blob' 
-							}; */
-					axios.defaults.headers = {
-					        'Content-Type': 'application/json', 
-					        'Cookie': 'abcdrashmi' 
-					    };
-				axios.post(`https://soa.actcorp.in/api/chatbot/authCustomer`, params).then((result) => {
-					
-					console.log(result.headers);
-					 console.log(result.headers['set-cookie']);
-					 var cookiee = [];
-		             var cookieValue = result.headers['set-cookie']; 
-		             console.log(cookieValue); 
-		             
-		            
-					
-					var rr={};
-					var resultarray = result.data;
-					console.log(resultarray);  
-					var fullfilment = resultarray.fulfillment;
-					if (resultarray.code == 200 && (resultarray.status)) {
-						fullfilment.forEach(element => {
-							var messages = element.messages;
-
-							messages.forEach(element => { 
-								var rr = {
-										"messages": [
-											{
-												"platform": "FACEBOOK",
-												"quickReplies": {
-													"quickReplies": element.replies,
-													"title": element.title
-												}
-											}
-											]	
-								};
-
-								let responseJson = {};
-								responseJson.fulfillmentMessages = rr.messages;
-								console.log('Response to Dialogflow: ' + JSON.stringify(responseJson));
-								response.json(responseJson);
-							});
-						});
-
-					} else if (resultarray.code == 200 && (!resultarray.status) ){
-						var r={};
-						agent.setContext({name: 'account_no', lifespan: 2, parameters: { value: false }});
-						fullfilment.forEach(element => { 
-							var messages = element.messages;
-							messages.forEach(element => {
-								/*var r = {
-										"messages": [
-											{
-												"text": { 
-													"text": [
-														element.speech
-														]
-												},
-												"platform": "FACEBOOK"
-											}
-											]      
-								};*/
-
-								var r = [
-									{
-										"text": { 
-											"text": [
-												element.speech
-												]
-										},
-										"platform": "FACEBOOK"
-									}
-									] ;
-								let responseJson = {};
-								responseJson = { fulfillmentMessages: r }; 
-								console.log('Response to Dialogflow: ' + JSON.stringify(responseJson));
-								response.json(responseJson);
-
-
-							});
-
-						});
-
-					}
-					else{
-						//agent.add('something went wrong');
-                      console.log('something went wrong');
-					}
-				}).catch(error => {
-					//agent.add(`Api is not working`);
-				});
-			} else if (request.body.originalDetectIntentRequest.source == 'google') {
-				//agent.add(`Hey, Im your Act Google Assistant`);
-	            console.log('Hey, Im your Act Google Assistant');
-			}
-		}); 
-		
-
-
-
-	}
-
-	function welcome(agent) { 
-		return new Promise(function (resolve, reject) {
-			firstapi();
-		});
-
-	}
-
-	function internet(agent) {
-
-		var parameter=request.body.queryResult.parameters;
-		var params={};
-		if(parameter.input_param == ''){
-			agent.setContext({name: 'account_no', lifespan: 2, parameters: { value: false }});
-			agent.add('Please enter your account number/username/registered mobile number');
-		}else{
-			var acc=  parameter.input_param;
-			var accno=acc.toString();
-			params = { source: request.body.originalDetectIntentRequest.source,data:[{input_param:accno}] };
-			return new Promise(function (resolve, reject) {
-				secondapi(agent);
-			});
-
-		}
-
-
-	}
-	
-
-	function slowspeed(agent){  
-		
-		agent.setContext({name: 'ci_session', lifespan: 4, parameters: {value: 'nnnnn'}});
-			var r= [
-					{
-						"text": { 
-							"text": [ 
-								'iiiiiiiiiiiiiiiiiiiiiiiiiiii'
-								]
-						},
-						"platform": "FACEBOOK"
-					}
-					]    ;  
-			
-			
-		 
-
-		let responseJson = {};
-		responseJson = { fulfillmentMessages: r };
-		console.log('Response to Dialogflow: ' + JSON.stringify(responseJson));
-		response.json(responseJson);
-	}
-	
-	
-
-		
-
-	function sendResponse(responseToUser) {
-		if (typeof responseToUser === 'string') {
-			let responseJson = { fulfillmentText: responseToUser }; // displayed
-			// response
-			response.json(responseJson); // Send response to Dialogflow
-
-		} else { 
-			let responseJson = {};
-			// Define the text response
-			responseJson.fulfillmentText = responseToUser.fulfillmentText;
-
-			if (responseToUser.messages) {
-				responseJson.fulfillmentMessages = responseToUser.messages;
-			}
-
-			if (responseToUser.outputContexts) {
-				responseJson.outputContexts = responseToUser.outputContexts;
-				// agent.setContext(responseToUser.outputContexts);
-			}
-			console.log('Response to Dialogflow: ' + JSON.stringify(responseJson));
-			response.json(responseJson);
-		}
- 
-	}
-
-
-	let intentMap = new Map();
-	intentMap.set('Default Welcome Intent', welcome);
-	intentMap.set('Default Fallback Intent', fallback);
-	intentMap.set('Internet not working', internet); 
-	intentMap.set('SlowSpeed', slowspeed);  
-
-	agent.handleRequest(intentMap);
+  var speechResponse = {
+    google: {
+      expectUserResponse: true,
+      richResponse: {
+        items: [
+          {
+            simpleResponse: {
+              textToSpeech: speech
+            }
+          }
+        ]
+      }
+    }
+  };
+  
+  return res.json({
+    payload: speechResponse,
+    //data: speechResponse,
+    fulfillmentText: speech,
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
 });
+
+restService.post("/audio", function(req, res) {
+  var speech = "";
+  switch (req.body.result.parameters.AudioSample.toLowerCase()) {
+    //Speech Synthesis Markup Language 
+    case "music one":
+      speech =
+        '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
+      break;
+    case "music two":
+      speech =
+        '<speak><audio clipBegin="1s" clipEnd="3s" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
+      break;
+    case "music three":
+      speech =
+        '<speak><audio repeatCount="2" soundLevel="-15db" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
+      break;
+    case "music four":
+      speech =
+        '<speak><audio speed="200%" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
+      break;
+    case "music five":
+      speech =
+        '<audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio>';
+      break;
+    case "delay":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    //https://www.w3.org/TR/speech-synthesis/#S3.2.3
+    case "cardinal":
+      speech = '<speak><say-as interpret-as="cardinal">12345</say-as></speak>';
+      break;
+    case "ordinal":
+      speech =
+        '<speak>I stood <say-as interpret-as="ordinal">10</say-as> in the class exams.</speak>';
+      break;
+    case "characters":
+      speech =
+        '<speak>Hello is spelled as <say-as interpret-as="characters">Hello</say-as></speak>';
+      break;
+    case "fraction":
+      speech =
+        '<speak>Rather than saying 24+3/4, I should say <say-as interpret-as="fraction">24+3/4</say-as></speak>';
+      break;
+    case "bleep":
+      speech =
+        '<speak>I do not want to say <say-as interpret-as="bleep">F&%$#</say-as> word</speak>';
+      break;
+    case "unit":
+      speech =
+        '<speak>This road is <say-as interpret-as="unit">50 foot</say-as> wide</speak>';
+      break;
+    case "verbatim":
+      speech =
+        '<speak>You spell HELLO as <say-as interpret-as="verbatim">hello</say-as></speak>';
+      break;
+    case "date one":
+      speech =
+        '<speak>Today is <say-as interpret-as="date" format="yyyymmdd" detail="1">2017-12-16</say-as></speak>';
+      break;
+    case "date two":
+      speech =
+        '<speak>Today is <say-as interpret-as="date" format="dm" detail="1">16-12</say-as></speak>';
+      break;
+    case "date three":
+      speech =
+        '<speak>Today is <say-as interpret-as="date" format="dmy" detail="1">16-12-2017</say-as></speak>';
+      break;
+    case "time":
+      speech =
+        '<speak>It is <say-as interpret-as="time" format="hms12">2:30pm</say-as> now</speak>';
+      break;
+    case "telephone one":
+      speech =
+        '<speak><say-as interpret-as="telephone" format="91">09012345678</say-as> </speak>';
+      break;
+    case "telephone two":
+      speech =
+        '<speak><say-as interpret-as="telephone" format="1">(781) 771-7777</say-as> </speak>';
+      break;
+    // https://www.w3.org/TR/2005/NOTE-ssml-sayas-20050526/#S3.3
+    case "alternate":
+      speech =
+        '<speak>IPL stands for <sub alias="indian premier league">IPL</sub></speak>';
+      break;
+  }
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
+});
+
+restService.post("/video", function(req, res) {
+  return res.json({
+    speech:
+      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    displayText:
+      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    source: "webhook-echo-sample"
+  });
+});
+
+restService.post("/slack-test", function(req, res) {
+  var slack_message = {
+    text: "Details of JIRA board for Browse and Commerce",
+    attachments: [
+      {
+        title: "JIRA Board",
+        title_link: "http://www.google.com",
+        color: "#36a64f",
+
+        fields: [
+          {
+            title: "Epic Count",
+            value: "50",
+            short: "false"
+          },
+          {
+            title: "Story Count",
+            value: "40",
+            short: "false"
+          }
+        ],
+
+        thumb_url:
+          "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
+      },
+      {
+        title: "Story status count",
+        title_link: "http://www.google.com",
+        color: "#f49e42",
+
+        fields: [
+          {
+            title: "Not started",
+            value: "50",
+            short: "false"
+          },
+          {
+            title: "Development",
+            value: "40",
+            short: "false"
+          },
+          {
+            title: "Development",
+            value: "40",
+            short: "false"
+          },
+          {
+            title: "Development",
+            value: "40",
+            short: "false"
+          }
+        ]
+      }
+    ]
+  };
+  return res.json({
+    speech: "speech",
+    displayText: "speech",
+    source: "webhook-echo-sample",
+    data: {
+      slack: slack_message
+    }
+  });
+});
+
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
+
+    © 2020 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Help
+
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
